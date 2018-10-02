939e68c52fc80b18fd31baab71cac84797a6bff8
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-02 10:57:33 +0530

    added routes for login and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../user/login/login.component';
+import { componentFactoryName } from '@angular/compiler';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login', component: LoginComponent, pathMatch: 'full'
+      },
+      {
+        path: '', component: LoginComponent, pathMatch: 'full'
+      },
+      {
+        path: '*', component: LoginComponent
+      },
+      {
+        path: '**', component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: 'signup', component: SignupComponent
+      }
+    ]),
+    FormsModule
   ],
-  declarations: []
+  declarations: [LoginComponent, SignupComponent]
 })
 export class UserModule { }
