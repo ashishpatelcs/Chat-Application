d763303f6ccdf796a0037d6769031dc6f2a90c2b
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-05 18:07:34 +0530

    implemented CanActivate in route guard service

--- src/app/chat/chat-route-guard.service.ts
+++ src/app/chat/chat-route-guard.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 @Injectable()
 export class ChatRouteGuardService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    console.log('Router guard service is running...');
+  }
 }
