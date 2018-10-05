6c175961dfb1e20c7ddf8d24303168ad6380110c
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-05 18:05:22 +0530

    added chat route guard service

--- src/app/chat/chat-route-guard.service.ts
+++ src/app/chat/chat-route-guard.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ChatRouteGuardService {
 
   constructor() { }
